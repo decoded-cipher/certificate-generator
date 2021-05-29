66d1a5155245a9ad1e4cbaf85449bac8feab7c05
Arjun Krishna <arjun12345krishna@gmail.com>
2021-05-30 03:08:47 +0530

    Started Designing the Page

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var hbs = require('express-handlebars');
+
+var app = express();
+
+app.set('views', (__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs({
+    extname: 'hbs',
+    defaultLayout: 'index',
+    layoutsDir: __dirname + '/views/layout/'
+}));
+
+app.use(express.static(__dirname, +'public'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(3000);
